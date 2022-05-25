e05048f870613a7d3bb7bbffba4a670b4675b314
jhwook <jhwooook@naver.com>
2022-05-25 17:11:24 +0900

    google-login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
-import { GoogleLogin } from 'react-google-login';
-
-const responseGoogle = (response) => {
-  console.log(response);
-};
 
 function App() {
+  const [user, setUser] = useState('');
+
+  console.log(user);
+
+  useEffect(() => {
+    googleLogin();
+  }, []);
+
+  const getCookieValue = (key) => {
+    let cookieKey = key + '=';
+    let result = '';
+    const cookieArr = document.cookie.split(';');
+
+    for (let i = 0; i < cookieArr.length; i++) {
+      if (cookieArr[i][0] === ' ') {
+        cookieArr[i] = cookieArr[i].substring(1);
+      }
+
+      if (cookieArr[i].indexOf(cookieKey) === 0) {
+        result = cookieArr[i].slice(cookieKey.length, cookieArr[i].length);
+        return result;
+      }
+    }
+    return result;
+  };
+
+  const googleLogin = async () => {
+    const accessToken = getCookieValue('accessToken');
+    axios
+      .get('http://localhost:3000/users/auth', {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((res) => {
+        console.log(res);
+        setUser(res.data);
+      });
+  };
+
   return (
-    <GoogleLogin
-      clientId="511516218336-9gbsps8s7q5kqvr4jrno5e9nemp5a14t.apps.googleusercontent.com"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy="single_host_origin"
-    />
+    <div>
+      <a href="http://localhost:3000/users/google">google</a>
+    </div>
   );
 }
 
